0f4103105d5b33a1fa9e532d180bb3acb475fb43
Suresh_Gobi <sureshgobi34@gmail.com>
2024-06-03 12:11:20 +0530

    bug fixed

--- obj/Debug/net8.0/ebookings.AssemblyInfo.cs
+++ obj/Debug/net8.0/ebookings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ebookings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489b6d4380b09472008cf2a4c70b89774a866b2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17eb05c6710e3e1f3a15590ec02559e0718f3da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ebookings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ebookings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
